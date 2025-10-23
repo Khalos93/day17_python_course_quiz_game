cff76547f5153e5e4f2030a4f589bda940a16812
Baby Tiger <r.golin@hotmail.it>
2025-10-23 23:31:35 +0100

    adjust engine creation outside of main loop. create external loop to restart the game when user fail a question

--- main.py
+++ main.py
@@ -7,13 +7,20 @@ questions_bank = []
 for data in question_data:
     question = Question(data['text'], data['answer'])
     questions_bank.append(question)
-    quiz_engine = QuizEngine(questions_bank)
 
+quiz_engine = QuizEngine(questions_bank)
 while True:
-    response = quiz_engine.next_question()
-    if quiz_engine.is_answer_correct(response):
-        quiz_engine.increase_question_number()
-    else:
-        print("You are wrong.")
-        print(f"Your final score is {quiz_engine.score}")
+    play_game = str(input("Welcome to this trivia game! "
+                          "Your goal is figuring out given a question if it is True or False!\n"
+                          "Here goes the first question, Are you ready to play? Type Y or N")).strip().lower()
+    if play_game == 'n':
         break
+
+    while True:
+        response = quiz_engine.next_question()
+        if quiz_engine.is_answer_correct(response):
+            quiz_engine.increase_question_number()
+        else:
+            print("You are wrong.")
+            print(f"Your final score is {quiz_engine.score}")
+            break
